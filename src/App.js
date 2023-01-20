c3bf08d9ca8bb517dffac35028004874ef9ee9a2
Bradley Griffin <bigriff11@gmail.com>
2023-01-20 08:14:43 -0700

    Import in the dad joke function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import "./App.css";
-
 function App() {
+  let getDadJoke = async () => {
+    let response = await fetch(
+      "https://api.api-ninjas.com/v1/dadjokes?limit=1",
+      {
+        method: "GET",
+        headers: { "x-api-key": "QgE8WQv4+gZfs87KSWN3yA==fVEa3IiYV9onHw9X" },
+      }
+    );
+    let data = await response.json();
+    let joke = data[0];
+    console.log(joke.joke);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -16,6 +27,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={getDadJoke}>TEST</button>
       </header>
     </div>
   );
