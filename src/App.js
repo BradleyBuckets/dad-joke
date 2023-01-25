8ec7be9c8727d9bd917ba1496b50b9f69d972b45
Bradley Griffin <bigriff11@gmail.com>
2023-01-25 15:02:23 -0700

    Add another cross and adjust

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ function App() {
       <div className="cross five"></div>
       <div className="cross six"></div>
       <div className="cross seven"></div>
+      <div className="cross eight"></div>
       <div className="line l1"></div>
       <div className="line l2"></div>
       <div className="line l3"></div>
